a873b8fcf3e0dc18ce9c16a35c67ba66f3dbe364
MarceloCC <MarceloCC@hdi00520363.hdi.br>
2014-07-15 13:49:22 -0300

    dev

--- application/views/noticias.php
+++ application/views/noticias.php
@@ -6,10 +6,10 @@
 <div>
     <div>
         <figure id="slide">
-            <img class="ativo" alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[0]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[0]->st_imagem_1)) ?>" width="620" height="310"/>
-            <img alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[1]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[1]->st_imagem_1)) ?>" width="620" height="310"/>
-            <img alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[2]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[2]->st_imagem_1)) ?>" width="620" height="310"/>
-            <img alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[3]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[3]->st_imagem_1)) ?>" width="620" height="310"/>
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[0]->cd_noticia?>"><img class="ativo" alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[0]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[0]->st_imagem_1)) ?>" width="620" height="310"/></a>
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[1]->cd_noticia?>"><img alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[1]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[1]->st_imagem_1)) ?>" width="620" height="310"/></a>
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[2]->cd_noticia?>"><img alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[2]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[2]->st_imagem_1)) ?>" width="620" height="310"/></a>
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[3]->cd_noticia?>"><img alt="<?php echo iconv("utf-8", "iso-8859-1", $noticias[3]->st_noticia); ?>" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[3]->st_imagem_1)) ?>" width="620" height="310"/></a>
         </figure>
         <script lang="text/javascript">
             $(document).ready(function(){
@@ -32,25 +32,40 @@
     </div>
     <div>
         <div class="noticia_quadro">
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[4]->cd_noticia?>">
             <img alt="No Image" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[4]->st_imagem_1)) ?>" width="420" height="220"/>
             <h2><?php echo iconv("utf-8", "iso-8859-1", $noticias[4]->st_noticia); ?></h2>
-            <div><?php echo iconv("utf-8", "iso-8859-1", $noticias[4]->st_fonte); ?></div>
+            <p>
+                <?php echo iconv('utf-8', 'iso-8859-1', $noticias[4]->st_subtitulo);?>&nbsp;|&nbsp;
+                <?php echo $noticias[4]->dt_noticia;?>
+            </p>
             <p><?php echo substr(mb_convert_encoding((string)$noticias[4]->st_descricao, "iso-8859-1", "auto"), 0, 512); ?>...</p>
+            <div>
+                <?php echo iconv('utf-8', 'iso-8859-1', $noticias[4]->st_autor);?> | 
+                <a href="<?php echo iconv('utf-8', 'iso-8859-1', $noticias[4]->st_link);?>"><?php echo iconv("utf-8", "iso-8859-1", $noticias[4]->st_fonte); ?></a>
+            </div>
+            </a>
         </div>
         <div class="noticia_quadro">
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[5]->cd_noticia?>">
             <img alt="No Image" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[5]->st_imagem_1)) ?>" width="420" height="220"/>
             <h2><?php echo iconv("utf-8", "iso-8859-1", $noticias[5]->st_noticia); ?></h2>
             <p><?php echo substr(mb_convert_encoding((string) $noticias[5]->st_descricao, "iso-8859-1", "auto"), 0, 512); ?>...</p>
+            </a>
         </div>
         <div class="noticia_quadro">
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[6]->cd_noticia?>">
             <img alt="No Image" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[6]->st_imagem_1)) ?>" width="420" height="220"/>            
             <h2><?php echo iconv("utf-8", "iso-8859-1", $noticias[6]->st_noticia); ?></h2>
             <p><?php echo substr(mb_convert_encoding((string)$noticias[6]->st_descricao, "iso-8859-1", "auto"), 0, 512); ?>...</p>
+            </a>
         </div>
         <div class="noticia_quadro">
+            <a href="<?php echo base_url(); ?>noticias/detalhe/<?php echo $noticias[7]->cd_noticia?>">
             <img alt="No Image" src="<?php echo base_url(); ?>imagem/load/<?php echo urlencode(base64_encode($noticias[7]->st_imagem_1)) ?>" width="420" height="220"/>
             <h2><?php echo iconv("utf-8", "iso-8859-1", $noticias[7]->st_noticia); ?></h2>            
             <p><?php echo substr(mb_convert_encoding((string)$noticias[7]->st_descricao, "iso-8859-1", "auto"), 0, 512); ?>...</p>
+            </a>
         </div>
         <div class="limpa"></div>
     </div>
